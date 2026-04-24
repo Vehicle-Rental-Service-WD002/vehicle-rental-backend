9f5bb4faf1f7333643c19c4a8258bac093177d28
nidulaNemsith <nemsithbaduge215@gmail.com>
2026-04-24 11:48:39 +0530

    Initial Project Structure

---
+++ src/main/java/edu/wd12/vehicle_rental_backend/VehicleRentalBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.wd12.vehicle_rental_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/wd12/vehicle_rental_backend/VehicleRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wd12.vehicle_rental_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
