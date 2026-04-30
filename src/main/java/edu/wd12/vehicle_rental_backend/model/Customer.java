80c6e854250bed6f6e26b3225a0b54f0a138a693
BoyaMudi <mudithmasb@gmail.com>
2026-04-30 15:53:51 +0530

    Refactor Customer class to remove inheritance from User and simplify structure

--- src/main/java/edu/wd12/vehicle_rental_backend/model/Customer.java
+++ src/main/java/edu/wd12/vehicle_rental_backend/model/Customer.java
@@ -1,33 +1,7 @@
 package edu.wd12.vehicle_rental_backend.model;
 
-public class Customer extends User {
+public class Customer  {
     private String licenceNumber;
-
-    public Customer(String userId, String name, String email, String password, String licenceNumber) {
-        super(userId, name, email, password);
-        this.licenceNumber = licenceNumber;
-    }
-
-    @Override
-    public void registerUser() {
-        // Implement registration logic here
-    }
-
-    @Override
-    public String viewProfile() {
-        // Implement profile viewing logic here
-        return null;
-    }
-
-    @Override
-    public void updateProfile() {
-        // Implement profile updating logic here
-    }
-
-    @Override
-    public void deleteProfile() {
-        // Implement profile deletion logic here
-    }
 
     public void updateLicenceNumber(String newLicenceNumber) {
         this.licenceNumber = newLicenceNumber;
