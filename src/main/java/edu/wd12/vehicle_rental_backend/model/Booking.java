dfa4b5274e734702fc82180efdf39ff339682163
nemsi <nemsithbaduge215@gmail.com>
2026-04-30 23:06:38 +0530

    Add Booking class with attributes for rental management

---
+++ src/main/java/edu/wd12/vehicle_rental_backend/model/Booking.java
@@ -0,0 +1,9 @@
+package edu.wd12.vehicle_rental_backend.model;
+
+public class Booking {
+    private String bookingId;
+    private String startDate;
+    private String endDate;
+    private String actualReturnDate;
+    private String status;
+}
