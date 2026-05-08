a4f80e763c004d3097f7b1358911f662dc190437
Tharinsa7788 <tharinsalaknadi@gmail.com>
2026-05-08 10:03:49 +0530

    complete staff component

---
+++ src/main/java/edu/wd12/vehicle_rental_backend/controller/AuthController.java
@@ -0,0 +1,22 @@
+package edu.wd12.vehicle_rental_backend.controller;
+
+import edu.wd12.vehicle_rental_backend.dto.LoginRequestDto;
+import edu.wd12.vehicle_rental_backend.dto.LoginResponseDto;
+import edu.wd12.vehicle_rental_backend.service.AuthUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    final AuthUserService authUserService;
+
+    @RequestMapping("login")
+    public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto) {
+        return authUserService.login(loginRequestDto);
+    }
+}
---
+++ src/main/java/edu/wd12/vehicle_rental_backend/dto/LoginRequestDto.java
@@ -0,0 +1,18 @@
+package edu.wd12.vehicle_rental_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
