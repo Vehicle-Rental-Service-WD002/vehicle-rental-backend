3faa21da80e6b62068f6965fb6fd52c900d87240
AshfanAhamed2004 <ashfanahamed2004@gmail.com>
2026-05-11 21:34:20 +0530

    Add VehicleDto class to encapsulate vehicle data transfer objects

---
+++ src/main/java/edu/wd12/vehicle_rental_backend/dto/VehicleDto.java
@@ -0,0 +1,16 @@
+package edu.wd12.vehicle_rental_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleDto {
+    private String brand;
+    private String model;
+    private double dailyRate;
+    private int year;
+    private String type;
+}
