b07539f51fb343dd6580f7362354ef6719cb245c
minidumihisara2005-ops <minidumihisara2005@gmail.com>
2026-05-18 15:52:05 +0530

    Implement getAll Method

--- src/main/java/edu/wd12/vehicle_rental_backend/service/ReceiptService.java
+++ src/main/java/edu/wd12/vehicle_rental_backend/service/ReceiptService.java
@@ -2,10 +2,13 @@ package edu.wd12.vehicle_rental_backend.service;
 
 import edu.wd12.vehicle_rental_backend.entity.ReceiptEntity;
 
+import java.util.List;
+
 public interface ReceiptService {
     ReceiptEntity generateReceipt(Long rentalId);
     ReceiptEntity viewReceipt(Long id);
     ReceiptEntity updateLateFee(Long id, double fee);
+    List<ReceiptEntity> getAllRecipts();
     void voidReceipt(Long id);
     double calculateFinalTotal(double baseCost, double lateFee);
 }
